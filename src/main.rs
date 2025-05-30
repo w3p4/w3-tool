65bbf6a9159196304ff38bbfc1484218cfff5f9c
mewpawee <pawee@alphainnovationslab.io>
2025-05-30 10:03:44 +0700

    feat: update  code

--- src/main.rs
+++ src/main.rs
@@ -28,24 +28,25 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
+    // TODO: accept many addresses
     let token_address = Address::parse_checksummed(&args.address, None)?;
-    // Spin up a forked Anvil node.
-    // Ensure `anvil` is available in $PATH.
-    let rpc_url = args.rpc_url;
-    let provider =
-        ProviderBuilder::new().connect_anvil_with_wallet_and_config(|anvil| anvil.fork(rpc_url))?;
+    // define rpc url
+    let rpc_url = args.rpc_url.parse()?;
+    let provider = ProviderBuilder::new().connect_http(rpc_url);
 
+    // TODO: loop through all addresses
     // Create a contract instance.
     let erc20 = IERC20::new(token_address, &provider);
 
-    // Multicall
+    // Multicall to get the token info
     let multicall = provider
         .multicall()
         .add(erc20.name())
         .add(erc20.symbol())
         .add(erc20.decimals());
     let (name, symbol, decimals) = multicall.aggregate().await?;
-    println!("{name}, {symbol}, {decimals}");
+    println!("{name}, {symbol}, {decimals}, {token_address}");
+    // TODO: write to json file
 
     Ok(())
 }
